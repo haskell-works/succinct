b31eb6de46aa517f003a641f66bdb71aa1e8705e
John Ky <john.ky@iohk.io>
2025-02-09 19:24:04 +1100

    Import hw-balancedparens

---
+++ components/succinct-balancedparens/HaskellWorks/Data/BalancedParens/Internal/RoseTree.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE DeriveGeneric #-}
+
+module HaskellWorks.Data.BalancedParens.Internal.RoseTree
+  ( RoseTree(..)
+  , toBools
+  , toBools'
+  , size
+  , depth
+  ) where
+
+import GHC.Generics
+
+newtype RoseTree = RoseTree
+  { children :: [RoseTree]
+  } deriving (Eq, Show, Generic)
+
+toBools :: RoseTree -> [Bool]
+toBools rt = toBools' rt []
+
+toBools' :: RoseTree -> [Bool] -> [Bool]
+toBools' (RoseTree cs) = (True:) . foldr (.) id (fmap toBools' cs) . (False:)
+
+size :: RoseTree -> Int
+size (RoseTree cs) = 1 + sum (fmap size cs)
+
+depth :: RoseTree -> Int
+depth (RoseTree cs) = 1 + maximum (fmap depth cs)
